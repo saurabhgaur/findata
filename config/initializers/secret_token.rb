c19a63e418d3e1683141905fe421a28926cf64a6
Saurabh Gaur <saurabhgaur@gmail.com>
2012-05-28 22:18:35 +0530

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findata::Application.config.secret_token = '2101de84d3bb0b2e5c1a83d14e155b8e1e338f68b835901e83993890643cfc227863395a435bba9c1cc81383e6f77c47e3f7fff79ceee4ceb0ce99bee5323024'

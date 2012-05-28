de4a9d2db6c3265e657b0849e066c14b25cdbbd6
Saurabh Gaur <saurabhgaur@gmail.com>
2012-05-28 23:05:47 +0530

    updated mailing configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Findata::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 25,
-    domain: "example.com",
+    domain: "findata.herokuapp.com",
     authentication: "plain",
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"]

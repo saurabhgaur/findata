8585b466286f7fe64ea1fee86ae01230e6369d24
Saurabh Gaur <saurabhgaur@gmail.com>
2012-07-07 08:56:46 +0530

    Change login such that users reach their details page as soon as they log in instead of the index page, did this using instructions from https://github.com/plataformatec/devise/wiki/How-to%3A-redirect-to-a-specific-page-on-successful-sign-in-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end

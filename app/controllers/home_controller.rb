b9de605ef841e4a38556c29cabf733620c1e29b8
Saurabh Gaur <saurabhgaur@gmail.com>
2012-07-07 09:17:54 +0530

    redirect signed in users from the home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
+    @current_user = current_user
+    if user_signed_in?
+    	redirect_to user_path(current_user)
+    end
   end
 end
